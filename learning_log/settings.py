3633305340ee0f48c1db6b5b25dda79a69278627
Sean Yeo <seansqyeo@gmail.com>
2019-09-11 11:16:00 -0700

    added Entry model

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -31,14 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # MY APPS
+    'learning_logs',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # MY APPS
-    'learning_logs',
+
 ]
 
 MIDDLEWARE = [
--- learning_logs/models.py
+++ learning_logs/models.py
@@ -9,3 +9,20 @@ class Topic(models.Model):
 	def __str__(self):
 		"""Return a string representation of the model."""
 		return self.text
+
+class Entry(models.Model):
+	"""something specific learned about a topic"""
+	topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+	text = models.TextField()
+	date_added = models.DateTimeField(auto_now_add=True)
+
+	class Meta:
+		verbose_name_plural = 'entries'
+
+	def __str__(self):
+		"""return a string representation of the model"""
+		return f"{self.text[:50]}..."
+	# def __init__(self, arg):
+	# 	super(Entry, self).__init__()
+	# 	self.arg = arg
+		
